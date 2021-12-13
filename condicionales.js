ed794a668754feefc91317cfdcb70ff0408dd45c
Jorgecc15 <92415699+Jorgecc15@users.noreply.github.com>
2021-12-13 18:49:57 -0500

    Update condicionales.js

--- condicionales.js
+++ condicionales.js
@@ -5,6 +5,16 @@ let nota1 = Number (prompt("Ingrese su primera nota"))
 let nota2 = Number (prompt("Ingrese su segunda nota"))
 let nota3 = Number (prompt("Ingrese su tercera nota"))
 let p = (parseFloat(nota1)+parseFloat(nota2)+parseFloat(nota3))/3;
+do{
+    nota1=parseInt(prompt("ingrese su primera nota de nuevo"))
+} while (isNaN (nota1))
+do{
+    nota2=parseInt(prompt("ingrese su primera nota de nuevo"))
+} while (isNaN (nota2))
+do{
+    nota2=parseInt(prompt("ingrese su primera nota de nuevo"))
+} while (isNaN (nota2))
+
 alert("Su promedio es: "+ p)
 if (p>6){
     imgHtml.src = urlcarafeliz
