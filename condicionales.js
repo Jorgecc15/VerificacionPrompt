db75555545be574582c14c41f0f316737365c7f2
Jorgecc15 <92415699+Jorgecc15@users.noreply.github.com>
2021-12-14 18:39:20 -0500

    Update condicionales.js

--- condicionales.js
+++ condicionales.js
@@ -1,7 +1,6 @@
 let urlcarafeliz = "https://w7.pngwing.com/pngs/636/645/png-transparent-smiley-emoticon-happy-face-icon-good-sign-symbol-character-cheerful.png"
 let urlcaratriste = "https://www.pikpng.com/pngl/m/371-3711139_cara-feliz-y-triste-png-deutsch-biginf-smiley.png"
 let imgHtml = document.querySelector("#imagencara")
-let p = (parseFloat(nota1)+parseFloat(nota2)+parseFloat(nota3))/3;
 do{
     nota1 = parseInt(prompt("ingrese su primera nota "))
 } while (isNaN (nota1))
@@ -11,7 +10,7 @@ do{
 do{
     nota3 = parseInt(prompt("ingrese su tercera nota "))
 } while (isNaN (nota3))
-
+let p = (parseFloat(nota1)+parseFloat(nota2)+parseFloat(nota3))/3;
 alert("Su promedio es: "+ p)
 if (p>6){
     imgHtml.src = urlcarafeliz
