eafd8e62c284991368b42e6e97f0ae808ce392ab
Jorgecc15 <92415699+Jorgecc15@users.noreply.github.com>
2022-03-08 15:23:14 -0500

    Update condicionales.js

--- condicionales.js
+++ condicionales.js
@@ -3,13 +3,13 @@ let urlcaratriste = "https://www.pikpng.com/pngl/m/371-3711139_cara-feliz-y-tris
 let imgHtml = document.querySelector("#imagencara")
 do{
     nota1 = parseInt(prompt("ingrese su primera nota "))
-} while (isNaN (nota1))
+} while (isNaN (nota1)|| nota1<0 || nota1>10)
 do{
     nota2 = parseInt(prompt("ingrese su segunda nota "))
-} while (isNaN (nota2))
+} while (isNaN (nota2) || nota2<0 || nota3>10)
 do{
     nota3 = parseInt(prompt("ingrese su tercera nota "))
-} while (isNaN (nota3))
+} while (isNaN (nota3) || nota3<0 || nota3>10)
 let p = (parseFloat(nota1)+parseFloat(nota2)+parseFloat(nota3))/3;
 alert("Su promedio es: "+ p)
 if (p>6){
